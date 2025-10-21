0f28b4a64d7b19bafc35d060b070831534606c75
Asti22 <uun3461@gmail.com>
2025-10-21 10:15:54 +0700

    Menampilkan data semua mahasiswa dari database lewat endpoint `/api/mahasiswa`.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { dnsPrefetchControl } = require('helmet');
 let mysql = require('mysql2');
 const app = express();
 const port = 3000;
@@ -12,6 +13,36 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
     });
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Asti22##',
+    database: 'Biodata',
+    port: 3306
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        return;
+    }
+    console.log('Connected to database.');
+});
+
+app.get('api/mahasiswa', (req, res) => {
+    db.query('SELECT * FROM mahasiswa', (err, result)=>{
+        if(err) {
+            console.error('error executing query:' + err.stack);
+            res.status(500).send('error fetching users');
+            return;
+        }
+        res.json(results);
+    });
+});
+
+app.post( )
+
 
 
 
